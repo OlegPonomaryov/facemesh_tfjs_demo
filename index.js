fd0f5f52a45fa434418041b4ac449c7a5bb68ae5
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2021-03-18 22:25:13 +0200

    Image preprocessing and face detection

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const backendOutput = document.getElementById("backendOutput");
 
 async function main() {
     load_settings();
-    
-    let net = await faceLandmarksDetection.load(
-      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh,
-      {
-        shouldLoadIrisModel: false,
-        maxFaces: 1
-      });
-    
+
+
+    const faceDetModel = await tf.loadGraphModel("https://tfhub.dev/tensorflow/tfjs-model/blazeface/1/default/1", { fromTFHub: true });
+                                             
     const webcam = await tf.data.webcam(inputVideo);
 
-    let videoWidth = inputVideo.videoWidth,
-        videoHeight = inputVideo.videoHeight;
+    const videoWidth = inputVideo.videoWidth,
+          videoHeight = inputVideo.videoHeight;
     outputCanvas.width = videoWidth;
     outputCanvas.height = videoHeight;
+
+    const faceDetSize = videoHeight > videoWidth ? [128, Math.round(videoWidth * 128 / videoHeight)] :
+      (videoWidth > videoHeight ?[Math.round(videoHeight * 128 / videoWidth), 128] : [128, 128]);
+
+    console.log(faceDetSize);
+    
+    const faceDetPadding = [[Math.ceil((128 - faceDetSize[0]) / 2), Math.floor((128 - faceDetSize[0]) / 2)],
+                            [Math.ceil((128 - faceDetSize[1]) / 2), Math.floor((128 - faceDetSize[1]) / 2)],
+                            [0, 0]]
+    console.log(faceDetPadding);
 
     outputCanvasContext.font = "18px Arial MS";
 
@@ -27,13 +33,12 @@ async function main() {
         prev_frame_time = -1;
     while (true) {
       const img = await webcam.capture();
-      const predictions = await net.estimateFaces({
-        input: img,
-        predictIrises: false
-      });
+
+      const faceRect = await get_face_rect(faceDetModel, img, faceDetSize, faceDetPadding);
+      //console.log(faceRect);
 
       outputCanvasContext.drawImage(inputVideo, 0, 0);
-      plot_landmarks(predictions);
+      //plot_landmarks(predictions);
 
       let curr_frame_time = Date.now();
       if (prev_frame_time >= 0) {
@@ -55,6 +60,27 @@ function load_settings() {
   let backend = url.searchParams.get("back") ?? "webgl";
   tf.setBackend(backend);
   backendOutput.innerText = "Backend: " + tf.getBackend();
+}
+
+async function get_face_rect(faceDetModel, img, size, padding) {
+  const faceDetInput = tf.tidy(() => {
+    const scale = tf.scalar(127.5);
+    const offset = tf.scalar(1);
+    const result = img.resizeBilinear(size).div(scale).sub(offset).pad(padding, 0).reshape([1, 128, 128, 3]);
+    return result;
+  });
+    
+  let predictions = faceDetModel.predict(faceDetInput);
+  
+  const bestRect = tf.tidy(() => {
+    predictions = predictions.squeeze();
+    const logits = predictions.slice([0, 0], [-1, 1]).squeeze();
+    const bestRectIDX = logits.argMax();
+    const bestPred = predictions.gather(bestRectIDX).squeeze();
+    const bestRect = bestPred.slice(0, 5);
+    return bestRect;
+  });
+  return await bestRect.data();
 }
 
 function plot_landmarks(predictions) {
