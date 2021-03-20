7382311b293e721056025b3a779c77968f78a382
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2021-03-20 17:42:30 +0200

    Fix memory leak, predictions lag and low FPS

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function main(blazefaceAnchors) {
   outputCanvasContext.font = "18px Arial MS";
 
   const faceRectSize = Math.min(...sourceSize) / 2;
-  const faceRect = [1,
+  let faceRect = [1,
     Math.round(sourceSize[1] / 2 - faceRectSize / 2),
     Math.round(sourceSize[0] / 2 - faceRectSize / 2),
     Math.round(sourceSize[1] / 2 + faceRectSize / 2),
@@ -43,6 +43,9 @@ async function main(blazefaceAnchors) {
     plotFaceRect(faceRect);
     if (faceMesh[0] > 0.5) {
       plotLandmarks(faceMesh[1]);
+      let approxFaceRect = faceMesh[2];
+      padRect(approxFaceRect, sourceSize, 0.25);
+      faceRect = approxFaceRect;
     }
 
     let currFrameTime = Date.now();
@@ -77,11 +80,28 @@ function plotFaceRect(faceRect) {
   outputCanvasContext.stroke();
 }
 
+function padRect(rect, sourceSize, scale) {
+  const widthPad = Math.round((rect[3] - rect[1]) * scale),
+    heightPad = Math.round((rect[4] - rect[2]) * scale);
+  for (let i = 1; i < 5; i++) {
+    rect[i] = Math.round(rect[i]);
+  }
+
+  rect[1] -= widthPad;
+  rect[3] += widthPad;
+  rect[2] -= heightPad;
+  rect[4] += heightPad;
+
+  rect[1] = Math.max(0, rect[1]);
+  rect[2] = Math.max(0, rect[2]);
+  rect[3] = Math.min(sourceSize[1] - 1, rect[3]);
+  rect[4] = Math.min(sourceSize[0] - 1, rect[4]);
+}
 
 async function detectFaceMesh(faceMeshModel, img, faceRect) {
   const faceMeshInput = tf.tidy(() => preprocessFaceMesh(img, faceRect));
 
-  const predictions = await faceMeshModel.predict(faceMeshInput);
+  const predictions = await Promise.all(faceMeshModel.predict(faceMeshInput));
   faceMeshInput.dispose();
 
   const resultTensors = tf.tidy(() => postprocessFaceMesh(predictions, faceRect));
@@ -97,22 +117,23 @@ async function detectFaceMesh(faceMeshModel, img, faceRect) {
   const faceProb = result[0][0];
   const faceMesh = result[1];
 
-  let faceRest = [1];
+  let approxFaceRect = [1];
   for (let i = 2; i < 6; i++) {
-    faceRect.push(result[i]);
+    approxFaceRect.push(result[i]);
   }
 
-  return [faceProb, faceMesh];
+  return [faceProb, faceMesh, approxFaceRect];
 }
 
-function preprocessFaceMesh(img, faceRect) {
+function preprocessFaceMesh(img, faceRect) { 
   const scale = tf.scalar(255);
-  const result = img
-    .slice([faceRect[2], faceRect[1], 0],
-      [faceRect[4] - faceRect[2], faceRect[3] - faceRect[1], -1])
-    .resizeBilinear([FACE_MESH_SIZE, FACE_MESH_SIZE])
-    .div(scale)
-    .reshape([1, FACE_MESH_SIZE, FACE_MESH_SIZE, 3]);
+  const batchedImg = img.reshape([1, img.shape[0], img.shape[1], img.shape[2]])
+  const boxes = [[faceRect[2] / img.shape[0], faceRect[1] / img.shape[1],
+                  faceRect[4] / img.shape[0], faceRect[3] / img.shape[1]]];
+  const boxInd = [0];
+  const cropSize = [FACE_MESH_SIZE, FACE_MESH_SIZE];
+  const result = tf.image.cropAndResize(
+    batchedImg, boxes, boxInd, cropSize).div(scale);
   return result;
 }
 
